22ac1479d0c1299b8388da5c6c8823f8932d9112
Tambir Ahmed <tambirahmed@Tambirs-MBP.fios-router.home>
2019-11-23 15:29:38 -0500

    Initial commit

---
+++ src/test/java/githubtest/LoginTest.java
@@ -0,0 +1,35 @@
+package githubtest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver");
+		driver = new FirefoxDriver();
+	}
+	@Test
+	public void doLogin()
+	{
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("abcd@gmail.com");
+		driver.findElement(By.id("Passwd")).sendKeys("abcd");
+		
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
